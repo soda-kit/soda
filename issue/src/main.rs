c6651f40b8d650d2f6a2ef0b883e48ca8d7c68d8
Taizeng Wu <wutaizeng@gmail.com>
2022-03-02 14:57:26 +0800

    improve: implement IssueService trait and handle error

--- issue/src/main.rs
+++ issue/src/main.rs
@@ -1,108 +1,57 @@
 use axum::{
+    async_trait,
     extract::{Extension, Path},
     http::StatusCode,
-    response::IntoResponse,
-    routing,
+    response::{IntoResponse, Response},
+    routing::{get, post},
     AddExtensionLayer, Json, Router,
 };
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::env;
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
-    let client = reqwest::Client::new();
-    let app = Router::new()
-        .route("/projects/:project_id/issues", routing::post(issues_create))
-        .route("/projects/:project_id/issues/:issue_id", routing::get(issues_get))
-        .layer(AddExtensionLayer::new(client));
+    if let (Ok(host), Ok(user), Ok(pass)) = (
+        env::var("JIRA_HOST"),
+        env::var("JIRA_USER"),
+        env::var("JIRA_PASS"),
+    ) {
+        let issue_service = Arc::new(JiraIssueService::new(host, Credentials::Basic(user, pass)))
+            as DynIssueService;
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on http://{}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        let app = Router::new()
+            .route("/projects/:project_id/issues", post(issues_create))
+            .route("/projects/:project_id/issues/:issue_id", get(issues_get))
+            .layer(AddExtensionLayer::new(issue_service));
+
+        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+        println!("Listening on http://{}", addr);
+        axum::Server::bind(&addr)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    }
 }
 
 async fn issues_create(
     Path(project_id): Path<String>,
-    Json(input): Json<Issue>,
-    Extension(client): Extension<reqwest::Client>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let create_issue = CreateIssue {
-        fields: Fields {
-            project: Project {
-                key: project_id.clone(),
-            },
-            issuetype: IssueType {
-                name: "Task".to_owned(),
-            },
-            summary: input.title.clone(),
-            description: input.body.clone().unwrap(),
-        },
-    };
-
-    let resp: CreateResponse = client
-        .post(format!(
-            "{}/rest/api/latest/issue",
-            env::var("JIRA_HOST").unwrap()
-        ))
-        .basic_auth(
-            env::var("JIRA_USER").unwrap(),
-            Some(env::var("JIRA_PASS").unwrap()),
-        )
-        .json(&create_issue)
-        .send()
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
-
-    let issue = Issue {
-        name: Some(format!("projects/{}/issues/{}", project_id, resp.key)),
-        title: input.title,
-        body: input.body,
-        owner: input.owner,
-        assignee: input.assignee,
-        labels: input.labels,
-    };
-
-    Ok((StatusCode::CREATED, Json(issue)))
+    Json(create_issue): Json<CreateIssue>,
+    Extension(issue_service): Extension<DynIssueService>,
+) -> Result<Json<Issue>, AppError> {
+    let issue = issue_service.create_issue(project_id, create_issue).await?;
+    Ok(issue.into())
 }
 
 async fn issues_get(
     Path((project_id, issue_id)): Path<(String, String)>,
-    Extension(client): Extension<reqwest::Client>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let resp: GetResponse = client
-        .get(format!(
-            "{}/rest/api/latest/issue/{}",
-            env::var("JIRA_HOST").unwrap(),
-            issue_id.clone(),
-        ))
-        .basic_auth(
-            env::var("JIRA_USER").unwrap(),
-            Some(env::var("JIRA_PASS").unwrap()),
-        )
-        .send()
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
-
-    let issue = Issue {
-        name: Some(format!("projects/{}/issues/{}", project_id, resp.key)),
-        title: resp.fields.summary,
-        body: Option::from(resp.fields.description),
-        owner: None,
-        assignee: Option::from(resp.fields.assignee.name),
-        labels: Option::from(resp.fields.labels),
-    };
-
-    Ok((StatusCode::OK, Json(issue)))
+    Extension(issue_service): Extension<DynIssueService>,
+) -> Result<Json<Issue>, AppError> {
+    let issue = issue_service.get_issue(project_id, issue_id).await?;
+    Ok(issue.into())
 }
 
 #[derive(Deserialize)]
@@ -115,8 +64,51 @@ struct GetResponse {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+enum AppError {
+    BadRequest,
+    Unauthorized,
+    NotFound,
+}
+
+impl From<IssueError> for AppError {
+    fn from(inner: IssueError) -> Self {
+        match inner {
+            IssueError::BadRequest => AppError::BadRequest,
+            IssueError::Unauthorized => AppError::Unauthorized,
+            IssueError::NotFound => AppError::NotFound,
+        }
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, error_mesage) = match self {
+            AppError::BadRequest => (StatusCode::BAD_REQUEST, "Bad Request"),
+            AppError::Unauthorized => (StatusCode::UNAUTHORIZED, "Unauthorized"),
+            AppError::NotFound => (StatusCode::NOT_FOUND, "Not Found"),
+        };
+
+        let body = Json(json!({ "error": error_mesage }));
+
+        (status, body).into_response()
+    }
+}
+
+#[async_trait]
+trait IssueService {
+    async fn create_issue(
+        &self,
+        project_id: String,
+        create_issue: CreateIssue,
+    ) -> Result<Issue, IssueError>;
+    async fn get_issue(&self, project_id: String, issue_id: String) -> Result<Issue, IssueError>;
+}
+
+type DynIssueService = Arc<dyn IssueService + Send + Sync>;
+
+#[derive(Serialize)]
 struct Issue {
-    name: Option<String>,
+    name: String,
     title: String,
     body: Option<String>,
     owner: Option<String>,
@@ -124,8 +116,120 @@ struct Issue {
     labels: Option<Vec<String>>,
 }
 
-#[derive(Serialize)]
+#[derive(Deserialize)]
 struct CreateIssue {
+    title: String,
+    body: Option<String>,
+    owner: Option<String>,
+    assignee: Option<String>,
+    labels: Option<Vec<String>>,
+}
+
+enum IssueError {
+    BadRequest,
+    Unauthorized,
+    NotFound,
+}
+
+enum Credentials {
+    Basic(String, String),
+}
+
+struct JiraIssueService {
+    host: String,
+    credentials: Credentials,
+    client: Client,
+}
+
+impl JiraIssueService {
+    fn new<H>(host: H, credentials: Credentials) -> Self
+    where
+        H: Into<String>,
+    {
+        Self {
+            host: host.into(),
+            credentials,
+            client: Client::new(),
+        }
+    }
+}
+
+#[async_trait]
+impl IssueService for JiraIssueService {
+    async fn create_issue(
+        &self,
+        project_id: String,
+        create_issue: CreateIssue,
+    ) -> Result<Issue, IssueError> {
+        let create_request = CreateRequest {
+            fields: Fields {
+                project: Project {
+                    key: project_id.clone(),
+                },
+                issuetype: IssueType {
+                    name: "Task".to_owned(),
+                },
+                summary: create_issue.title.clone(),
+                description: create_issue.body.clone().unwrap(),
+            },
+        };
+
+        let builder = self
+            .client
+            .post(format!("{}/rest/api/latest/issue", self.host));
+
+        let builder = match self.credentials {
+            Credentials::Basic(ref user, ref pass) => {
+                builder.basic_auth(user.to_owned(), Some(pass.to_owned()))
+            }
+        };
+
+        let resp: CreateResponse = builder
+            .json(&create_request)
+            .send()
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        let issue = Issue {
+            name: format!("projects/{}/issues/{}", project_id, resp.key),
+            title: create_issue.title,
+            body: create_issue.body,
+            owner: create_issue.owner,
+            assignee: create_issue.assignee,
+            labels: create_issue.labels,
+        };
+
+        Ok(issue)
+    }
+    async fn get_issue(&self, project_id: String, issue_id: String) -> Result<Issue, IssueError> {
+        let resp: GetResponse = self
+            .client
+            .get(format!("{}/rest/api/latest/issue/{}", self.host, issue_id))
+            .send()
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        let issue = Issue {
+            name: format!("projects/{}/issues/{}", project_id, resp.key),
+            title: resp.fields.summary,
+            body: Some(resp.fields.description),
+            owner: None,
+            assignee: Some(resp.fields.assignee.name),
+            labels: Some(resp.fields.labels),
+        };
+
+        Ok(issue)
+    }
+}
+
+#[derive(Serialize)]
+struct CreateRequest {
     fields: Fields,
 }
 
